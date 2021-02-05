6243736250a0e4eae93d6cda11800e82133d2c09
SebastianSaliva <sebastian.saliva@upr.edu>
2021-02-04 22:55:50 -0400

    yup

---
+++ test1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+ 
+    cout << "hello" ;
+
+    cout << "\n Yeah that makes a lot of sense" << endl; 
+
+    cout << "I mean yeah facts.\n" ;
+
+    cout << "yeahhhhhhhhhhh\n" ;
+    
+    cout << "Yo input your name: " ;
+    
+    string name;
+
+    cin >> name;
+
+    cout << "yeah " << name << " Is a dumbass name." ;
+}
